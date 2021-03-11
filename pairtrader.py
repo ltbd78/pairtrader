be3778a3243196a01c98d49006519f55f250bcb3
ltbd78 <ltbd78@gmail.com>
2021-03-11 13:35:28 -0500

    added get_df() functionality

--- pairtrader.py
+++ pairtrader.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import *
 
@@ -36,65 +37,74 @@ class PairTrader:
                     self.account.update_position('X', -n*self.b1, self.X[i]) # sell b1*X
                     stoploss = (-self.z_crit - self.z_sl)*self.sd[i] + self.ma[i]
                     takeprofit = (-self.z_crit + self.z_tp)*self.sd[i] + self.ma[i]
-                    info = {'spread': self.spread[i], 'stoploss': stoploss, 'takeprofit': takeprofit}
-                    self.logs.append((i, 'L', info))
+                    info = {'status': 'L', 'spread': self.spread[i], 'stoploss': stoploss, 'takeprofit': takeprofit, 'balance': self.account.total_balance}
+                    self.logs.append((i, info))
                 elif self.z_crit < self.z[i] < self.z_crit + self.z_sl: # short spread
                     self.account.update_position('Y', -n, self.Y[i]) # sell Y
                     self.account.update_position('X', n*self.b1, self.X[i]) # buy b1*X
                     stoploss = (self.z_crit + self.z_sl)*self.sd[i] + self.ma[i]
                     takeprofit = (self.z_crit - self.z_tp)*self.sd[i] + self.ma[i]
-                    info = {'spread': self.spread[i], 'stoploss': stoploss, 'takeprofit': takeprofit}
-                    self.logs.append((i, 'S', info))
+                    info = {'status': 'S', 'spread': self.spread[i], 'stoploss': stoploss, 'takeprofit': takeprofit, 'balance': self.account.total_balance}
+                    self.logs.append((i, info))
             else:
-                if self.logs[-1][1] == 'L':
-                    if self.spread[i] < self.logs[-1][2]['stoploss']:
+                last_info = self.logs[-1][1]
+                if last_info['status'] == 'L':
+                    if self.spread[i] < last_info['stoploss']:
                         self.account.update_position('Y', 'close', self.Y[i]) # sell Y
                         self.account.update_position('X', 'close', self.X[i]) # buy b1*X
-                        self.logs.append((i, 'SL', self.spread[i]))
-                    elif self.spread[i] > self.logs[-1][2]['takeprofit']:
+                        info = {'status': 'SL', 'spread': self.spread[i], 'balance': self.account.total_balance}
+                        self.logs.append((i, info))
+                    elif self.spread[i] > last_info['takeprofit']:
                         self.account.update_position('Y', 'close', self.Y[i]) # sell Y
                         self.account.update_position('X', 'close', self.X[i]) # buy b1*X
-                        self.logs.append((i, 'TP', self.spread[i]))
-                elif self.logs[-1][1] == 'S':
-                    if self.spread[i] > self.logs[-1][2]['stoploss']:
+                        info = {'status': 'TP', 'spread': self.spread[i], 'balance': self.account.total_balance}
+                        self.logs.append((i, info))
+                elif last_info['status'] == 'S':
+                    if self.spread[i] > last_info['stoploss']:
                         self.account.update_position('Y', 'close', self.Y[i]) # buy Y
                         self.account.update_position('X', 'close', self.X[i]) # sell b1*X
-                        self.logs.append((i, 'SL', self.spread[i]))
-                    elif self.spread[i] < self.logs[-1][2]['takeprofit']:
+                        info = {'status': 'SL', 'spread': self.spread[i], 'balance': self.account.total_balance}
+                        self.logs.append((i, info))
+                    elif self.spread[i] < last_info['takeprofit']:
                         self.account.update_position('Y', 'close', self.Y[i]) # buy Y
                         self.account.update_position('X', 'close', self.X[i]) # sell b1*X
-                        self.logs.append((i, 'TP', self.spread[i]))
+                        info = {'status': 'TP', 'spread': self.spread[i], 'balance': self.account.total_balance}
+                        self.logs.append((i, info))
         return self.account, self.logs
     
     def plot(self, type, figsize=(20, 10), zoom=False, markersize=78):
-        self.longs = [i[0] for i in self.logs if i[1] == 'L']
-        self.shorts = [i[0] for i in self.logs if i[1] == 'S']
-        self.stop_loss = [i[0] for i in self.logs if i[1] == 'SL']
-        self.take_profit = [i[0] for i in self.logs if i[1] == 'TP']
-        
+        self.longs = [i[0] for i in self.logs if i[1]['status'] == 'L']
+        self.shorts = [i[0] for i in self.logs if i[1]['status'] == 'S']
+        self.stop_loss = [i[0] for i in self.logs if i[1]['status'] == 'SL']
+        self.take_profit = [i[0] for i in self.logs if i[1]['status'] == 'TP']
         plot = plt.figure(figsize=figsize)
         plt.axvline(self.trainval_split, linestyle='--', color='black', label='Train/Val Split')
-        
         if type.lower() == 'spread':
             y = self.spread
             plt.plot(y, label='Spread')
             plt.plot(self.ma, linestyle='--', linewidth=.5, color='black', label='{} Tick Moving Average'.format(self.window))
             plt.plot(self.ma+self.z_crit*self.sd, linestyle='--', linewidth=.5, color='black', label='MA + {}*sigma'.format(self.z_crit))
             plt.plot(self.ma-self.z_crit*self.sd, linestyle='--', linewidth=.5, color='black', label='MA - {}*sigma'.format(self.z_crit))
-            
         if type.lower() == 'z':
             y = self.z
             plt.plot(y, label='Z Scores (Standarized Spread)')
             plt.axhline(self.z_crit, linestyle='--', linewidth=.5, color='black')
             plt.axhline(-self.z_crit, linestyle='--', linewidth=.5, color='black')
-            
         plt.scatter(self.longs, y[self.longs], marker=6, s=markersize, color='black', label='Long Spread')
         plt.scatter(self.shorts, y[self.shorts], marker=7, s=markersize, color='black', label='Short Spread')
         plt.scatter(self.stop_loss, y[self.stop_loss], marker='x', s=markersize, color='red', label='Stop Loss')
         plt.scatter(self.take_profit, y[self.take_profit], marker='o', s=markersize, color='green', label='Take Profit')
         plt.legend(loc=3)
-        
         if zoom:
             plt.xlim(self.trainval_split, len(y))
-            
         return plot
+    
+    def get_df(self):
+        idx = []
+        values = []
+        for i, j in self.logs:
+            idx.append(i)
+            values.append(j)
+        df = pd.DataFrame(values, index=idx)
+        df['diffs'] = df['balance'].diff()
+        return df
